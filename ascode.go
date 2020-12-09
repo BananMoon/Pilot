924e7f94d2e61fc58f4a2a7428c223c84f2c7128
BananMoon <myj7082@naver.com>
2020-12-09 12:29:41 +0900

    Update ascode.go

--- ascode.go
+++ ascode.go
@@ -1,5 +1,5 @@
 package main
-import(
+import (
 	"fmt"
 	"strconv"
 	"bytes"
@@ -20,11 +20,11 @@ func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) pb.Response 
 		return s.setWallet(APIstub, args)
 	} else if function == "getWallet" {	//지갑정보등록cc
 		return s.getWallet(APIstub, args)
-	} else if function == "addCoin" {
-		return s.addCoin(APIstub, args)
-	}/*else if function == "addCode" {	//악성코드 등록cc
+	} else if function == "addCode" {	//악성코드 등록cc
 		return s.addCode(APIstub, args)
-	}*/
+	} else if function == "addCoin" {	//평가시 코인지급cc
+		return s.addCoin(APIstub, args)	
+	}
 	fmt.Println("Please check your function : "+ function)
 	return shim.Error("Unknown function")
 }
@@ -37,21 +37,20 @@ func main() {
 
 //지갑 정보 등록 구조체
 type Wallet struct {
-	//User string `json:"user"`		//글쓴이 이름->필요한가?
-	WalletID string `json:"walletid"`
+	WalletID string `json:"walletid"`		//Q.WalletID량 ID랑 똑같은걸로 할까?
 	Token string `json:"token"`
 }
 //지갑 생성
 func (s *SmartContract) setWallet(stub shim.ChaincodeStubInterface, args []string) pb.Response {	//WalletID받으면 Token 0으로 시작
 
-	if len(args) != 2 {		//WalletID,Token=0
-		return shim.Error("Incorrect number of arguments. Expecting 2")
+	if len(args) != 1 {		//WalletID,Token=0
+		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
 
-	A := Wallet{WalletID: args[0], Token: args[1]}
+	A := Wallet{WalletID: args[0], Token: "0"}
 	AasJSONBytes, _ := json.Marshal(A)
 	err := stub.PutState(A.WalletID, AasJSONBytes)
-//	fmt.Println("Your WalletID :" + A.WalletID + ", Token : 0")	출력안된다..
+//	fmt.Println("Your WalletID :" + A.WalletID + ", Token : 0")	출력x
 
 	if err != nil {
 		return shim.Error("Failed to create wallet " + A.WalletID)
@@ -100,13 +99,12 @@ func (s *SmartContract) addCoin(stub shim.ChaincodeStubInterface, args []string)
 	var X int		//추가될 토큰양
 	var err error
 
-	if len(args) != 2 {		//WalletID, 지급할 coin양(addCode, 평가받을때)
+	if len(args) != 2 {		//WalletID
 		return shim.Error("Incorrect number of arguments. Expecting 2")
 	}
 
-	A=args[0]	//Id
-	X, _ = strconv.Atoi(args[1])
-
+	A = args[0]	//Id
+	X,_ = strconv.Atoi(args[1])
 	AAsBytes, err := stub.GetState(A)	//A의 정보
 	if err != nil {
 		return shim.Error(err.Error())
@@ -121,20 +119,20 @@ func (s *SmartContract) addCoin(stub shim.ChaincodeStubInterface, args []string)
 	updatedAAsBytes, _ := json.Marshal(walletA)
 	stub.PutState(args[0],updatedAAsBytes)
 
+
 	fmt.Printf("ID:"+walletA.WalletID+", Token: "+walletA.Token)
 	return shim.Success(nil)
 }
 
 //악성코드 구조체
 type Ascode struct{
-	Filehash string `json:"filehash"`	
+	URL string `json:"url"`	//Filehash SHA256 오류.
 	Uploader string `json:"uploader"`	//글쓴이이름
-	Time string `json:"time"`			
-	Ipfs string `json:"ipfs"`
+	Time string `json:"time"`			//int64?
+	//Ipfs string `json:"ipfs"`
 	Country string `json:"country`
 	Os string `json:"os"`
-//	Id string `json:"id"`			//자동생성->CodeId구조체에서 정의.
-	WalletID string `json:"walletid"`//
+	WalletID string `json:"walletid"`
 }
 
 //악성코드 고유번호 ->등록 시 ID로 자동설정
@@ -144,7 +142,7 @@ type CodeKey struct {
 }
 
 //악성코드 등록할때마다 사용되는 고유ID를 만드는 함수 generateId
-func (s *SmartContract) generateId(stub shim.ChaincodeStubInterface) []byte {		//[]byte: return할 형 적어줌.
+func generateId(stub shim.ChaincodeStubInterface) []byte {		//[]byte: return할 형 적어줌.
 	var isFirst bool = false	//첫번째인가?
 
 	codekeyAsBytes, err := stub.GetState("latestKey")	//악성코드의 마지막 키값으로 원장 조회
@@ -166,15 +164,15 @@ func (s *SmartContract) generateId(stub shim.ChaincodeStubInterface) []byte {		/
 		codekey.Idx = codekey.Idx + 1
 	}
 	fmt.Println("Last CodeKey is " + codekey.Key + " : " + tempIdx)
-	returnValueBytes,_ := json.Marshal(codekey)	//codeid를 json으로 변환
+	returnValueBytes,_ := json.Marshal(codekey)	//codekey를 json으로 변환
 
 	return returnValueBytes		//codeKey를 json형식으로 반환
 }
 
-//악성코드 등록 및 평가 addCode() ->addCoin을 호출해야함.
+//악성코드 등록 및 평가 addCode() ->addCoin을 호출
 func (s *SmartContract) addCode(APIstub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 7 {	//Filehash,Uploader,Time,Ipfs,Country,Os,WalletID
-		return shim.Error("Incorrect number of arguments. Expecting 7")
+	if len(args) != 6 {	//Filehash,Uploader,Time,Ipfs,Country,Os,WalletID
+		return shim.Error("Incorrect number of arguments. Expecting 6")
 	}
 
 	var codekey = CodeKey{}
@@ -182,7 +180,7 @@ func (s *SmartContract) addCode(APIstub shim.ChaincodeStubInterface, args []stri
 	keyidx := strconv.Itoa(codekey.Idx)
 	fmt.Println("Key :" + codekey.Key + ", Idx : " + keyidx)
 
-	var ascode = Ascode{Filehash: args[0], Uploader: args[1], Time: args[2], Ipfs: args[3], Country: args[4], Os: args[5], WalletID: args[6]}
+	var ascode = Ascode{URL: args[0], Uploader: args[1], Time: args[2], Country: args[3], Os: args[4], WalletID: args[5]}
 	codeAsJSONBytes, _ := json.Marshal(ascode) //구조체를 json으로 변환
 	//var idString = codeid.Key + keyidx
 	fmt.Println("AscodeID is "+keyidx)	//ID는 keyidx로만해서 숫자만. ex) 0 , 1 ..
@@ -193,10 +191,27 @@ func (s *SmartContract) addCode(APIstub shim.ChaincodeStubInterface, args []stri
 	codekeyAsBytes, _ := json.Marshal(codekey)		//구조체를 json형식으로 변환해서 codekeyAsBytes에 저장
 	APIstub.PutState("latestKey", codekeyAsBytes)	//codeis원장에 등록
 
-	//코인 발급 함수 addCoin() 호출.
-	var wallet = Wallet{}
-	json.Unmarshal(addCoin(APIstub,wallet.WalletID,"10"),&wallet)		//코드등록하면 10코인 부여
-	fmt.Println("Your coin is" + wallet.Token)
+	//코인 발급(글 기재시 10코인 발급)
+	var A string
+	var TokenA int	//토큰 보유양
+	var X int		//추가될 토큰양
 
+	A=args[5]	//Id
+	X=100
+
+	walletAsBytes, err := APIstub.GetState(args[5])	//A의 정보
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	wallet := Wallet{}
+	json.Unmarshal(walletAsBytes, &wallet)
+	TokenA, _ = strconv.Atoi(string(wallet.Token))
+	wallet.Token = strconv.Itoa(TokenA + X)
+	wallet.WalletID = wallet.WalletID
+	updatedAAsBytes, _ := json.Marshal(wallet)
+	APIstub.PutState(A,updatedAAsBytes)
+
+	fmt.Printf("ID:" + wallet.WalletID + ", Token: "+wallet.Token)
 	return shim.Success(nil)
 }
